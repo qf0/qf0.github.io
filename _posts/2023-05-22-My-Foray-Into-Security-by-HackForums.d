3be74220dbcfd8eea49adc89419dba0c3f269f3c
qf0 <56571091+qf0@users.noreply.github.com>
2023-05-22 13:15:40 -0500

    Create 2023-05-22-My-Foray-Into-Security-by-HackForums.d
    v0

---
+++ _posts/2023-05-22-My-Foray-Into-Security-by-HackForums.d
@@ -0,0 +1,25 @@
+---
+layout: post
+title: "My Foray Into Security by HackForums.net"
+date: 2023-05-22
+---
+
+Like others, I got in to computer security at least the web application/PC side by discovering HackForums.net (HF) by doing a Google search for a, "Hacking site". I quickly signedup in 2012 after lurking the forums for a while.
+What is a "RAT", or "botnet"? I wondered after seeing it on HF. Soon after, I had my Darkcomet stub, and a few infections by following a tutorial on HF with my home IP... I won't say my handles, or any other PII, because my old posts
+are embarrassing, and I was a young skid (script kiddie) back then. 
+
+Also, because I naively paid for "1337" which was a pay to get title on the forums with my real name, and the DB was dumped later on. Krebsonsecurity later doxed Malwaretech and linked him back to his HF profile by people posting his dox in a thread there,
+and by searching the HF dump. I was very gulible and naive back then, and was exploited a few times. However, at least I learned a lot, and it started my interest in computer security non related to video games.
+Of course there was some video game cheating sub forums which I enjoyed browsing.
+
+I befriended some people, which included a person connected to the Blackshades RAT which the FBI arrested. Also, Betamonkey (maker of betabotnet), and a few other botnet developers. Also I learned about "Booters" which would DDoS someone or a website offline,
+for a price. Usually a botnet with thousands of infected computers, and features like grabbing someone's IP from Skype/Cloudflare real IP grab. 
+There even were sellers selling infected computer "installs", which where infected computers running their malware, but they would install your malware on there as well for a lump sum.
+
+However, most "install" sellers would run their botnet's own anti-virus/malware cleaner to get rid of your malware, so they could have it for themselves again. I also learned about cryptocurrently,
+and other psuedoanonymous payment methods such as Liberty Reverse which was shut down by the DOJ and other entities due to money laundering. Also "Lizard squad" members were also active on HF.
+
+FBI recently posted on their front page that DDoS services such as Booters and "Stressers" aka legal guise similar to "remote support" RATs to sell illegal software/service that the ToS states is only for legal WhiteHat things.
+Which motivated me to write this blog post about HF. I learned about Exploit Kits, and I talked to a Russian government affilated seller of the orange exploit kit over XAMPP about renting it.
+
+Looking back, I'm glad I never did any real damage to anyone, and remedied my ethics and maturity issues shorty after joining HF. I hope others are living more productive lives that I used to talk to. BTW, all things stated here are past the statue of limitations, and I have repented.  
